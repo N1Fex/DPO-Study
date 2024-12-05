e7c7c8561595e1dbeb8530a98561a324b5b5d085
N1Fex <ponchikhanka@mail.ru>
2024-12-05 20:53:45 +1000

    Define colors

--- app/src/main/java/ru/fefu/dpo_study/ui/theme/Color.kt
+++ app/src/main/java/ru/fefu/dpo_study/ui/theme/Color.kt
@@ -2,10 +2,20 @@ package ru.fefu.dpo_study.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val LightBackground = Color(0xFFE0E0E0)
+val LightSurface = Color(0xFFFFFFFF)
+val LightPrimaryColor = Color(0xFF4B09F3)
+val LightSecondaryColor = Color(0xFFE1E1E1)
+val LightOnPrimaryColor = Color(0xFFFFFFFF)
+val LightOnSecondaryColor = Color(0xFF282828)
+val LightOnSurfaceColor = Color(0xFF282828)
+val LightActivityDateColor = Color(0xFF808080)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Цвета для темной темы
+val DarkBackground = Color(0xFF333333)
+val DarkSurface = Color(0xFF212121)
+val DarkPrimaryColor = Color(0xFF323232)
+val DarkSecondaryColor = Color(0xFF535353)
+val DarkOnPrimaryColor = Color(0xFFFFFFFF)
+val DarkOnSecondaryColor = Color(0xFFFFFFFF)
+val DarkOnSurfaceColor = Color(0xFFFFFFFF)
